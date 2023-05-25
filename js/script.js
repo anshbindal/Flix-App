f901c82536d4f0c61b89955ae084b6472828bb98
Ansh <ansh.bindal1996@gmail.com>
2023-05-25 11:46:53 +0530

    Popular movies added on HomePage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,49 @@
 const global = {
   currentPage: window.location.pathname,
 };
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("/movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+    console.log(div);
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+// Fetch data from TMDB API
+
+async function fetchAPIData(endpoint) {
+  const API_KEY = "97be2102b7145915496399b16c441076";
+  const API_URL = "https://api.themoviedb.org/3/";
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}`);
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight Active Nav Tab
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
@@ -15,7 +58,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
